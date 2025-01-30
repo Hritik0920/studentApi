13c5fd0a3a2ac204a191926a42b62947f2831e80
Hritik0920 <hritik30singh@gmail.com>
2025-01-30 19:28:46 +0530

    second commit

--- src/main/java/com/StudentAPI/StudentApIdemoApplication.java
+++ src/main/java/com/StudentAPI/StudentApIdemoApplication.java
@@ -2,10 +2,21 @@ package com.StudentAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+import com.StudentAPI.Model.Student;
 
 @SpringBootApplication
-public class StudentApIdemoApplication {
+public class StudentApIdemoApplication extends SpringBootServletInitializer{
 
+	@Override  
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+	{  
+	return application.sources(Student.class);  
+	}  
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApIdemoApplication.class, args);
 		System.out.println("Appliction Running");
